a4611b6b3d38c6c039de38d4c338099a228231dd
GL0CKck <78853462+GL0CKck@users.noreply.github.com>
2021-08-19 10:46:49 +0300

    Added forms for cart

--- cart/urls.py
+++ cart/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'cart'
 
 urlpatterns = [
-
+    path('',views.cart_detail,name='cart_detail'),
+    path('add/<int:pk>/',views.cart_add,name='cart_add'),
+    path('remove/<int:pk>/',views.cart_remove,name='cart_remove'),
 ]
